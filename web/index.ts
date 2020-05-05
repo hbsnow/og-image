3af90ae921cd4ce0ed2063074c45e85d8873d518
hbsnow <yuki.4uing@gmail.com>
2020-05-06 02:18:23 +0900

    feat: add myicon

--- web/index.ts
+++ web/index.ts
@@ -145,6 +145,7 @@ const imageLightOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
+    { text: 'hbsnow.dev', value: 'https://hbsnow.dev/assets/img/site-icons/icon.svg' },
 ];
 
 const imageDarkOptions: DropdownOption[] = [
@@ -152,6 +153,7 @@ const imageDarkOptions: DropdownOption[] = [
     { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg' },
     { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-white-logo.svg' },
     { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-bw-logo.svg' },
+    { text: 'hbsnow.dev', value: 'https://hbsnow.dev/assets/img/site-icons/icon.svg' },
 ];
 
 const widthOptions = [
